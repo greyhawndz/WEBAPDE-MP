553c176c88d699618307178c95139345c03b1e2f
William <paolo_dionio@yahoo.com.ph>
2015-03-25 17:00:48 +0800

    Update whatdo.sql

--- src/java/DB/whatdo.sql
+++ src/java/DB/whatdo.sql
@@ -30,10 +30,14 @@ CREATE TABLE `account` (
   `password` varchar(45) NOT NULL,
   `birthday` date DEFAULT NULL,
   `about_me` varchar(500) DEFAULT NULL,
+  `sex` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `age` int(11) NOT NULL,
+  `realname` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`accountID`),
   UNIQUE KEY `Name_UNIQUE` (`name`),
   UNIQUE KEY `accountID_UNIQUE` (`accountID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +46,7 @@ CREATE TABLE `account` (
 
 LOCK TABLES `account` WRITE;
 /*!40000 ALTER TABLE `account` DISABLE KEYS */;
-INSERT INTO `account` VALUES (1,'greyhawndz','1234','1995-11-06','I suck'),(2,'yellow guy','4321','1995-01-01','He suck');
+INSERT INTO `account` VALUES (1,'greyhawndz','1234','1995-11-06','I suck','Male','suck@gmail.com',19,NULL),(2,'yellow guy','4321','1995-01-01','He suck','Male','isuck@gmail.com',12,NULL),(7,'manto','1234','2002-01-01',NULL,'Male','manto@yahoo.com',20,NULL),(8,'johnpogi','secret','1996-05-12',NULL,'Male','johncaingles@gmail.com',18,NULL);
 /*!40000 ALTER TABLE `account` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -111,6 +115,36 @@ LOCK TABLES `review` WRITE;
 INSERT INTO `review` VALUES (1,'HELLO',5,'DLSU IS BEST',1,1);
 /*!40000 ALTER TABLE `review` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `visit`
+--
+
+DROP TABLE IF EXISTS `visit`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `visit` (
+  `idvisit` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `place` int(11) NOT NULL,
+  PRIMARY KEY (`idvisit`),
+  KEY `username_idx` (`username`),
+  KEY `placename_idx` (`place`),
+  KEY `place_idx` (`place`),
+  KEY `visit_place_idx` (`place`),
+  CONSTRAINT `visit_place` FOREIGN KEY (`place`) REFERENCES `place` (`placeID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `visit_username` FOREIGN KEY (`username`) REFERENCES `account` (`name`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `visit`
+--
+
+LOCK TABLES `visit` WRITE;
+/*!40000 ALTER TABLE `visit` DISABLE KEYS */;
+/*!40000 ALTER TABLE `visit` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -121,4 +155,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-03-18 23:42:39
+-- Dump completed on 2015-03-25 16:59:28
